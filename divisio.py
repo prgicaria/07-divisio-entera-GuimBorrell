b775e5fdb02fd843db28734256f25daeae3c7eb5
gborell <gborell@instituticaria.cat>
2024-10-17 09:51:31 +0200

    correcció

--- divisio.py
+++ divisio.py
@@ -1,7 +1,7 @@
-divisor = int(input("Entra el teu divisor: "))
-dividend = int(input("Entra el teu dividend"))
-Quocient = (dividend)//(divisor)
-Residu = (dividend)%(divisor)
+dividend = int(input("Entra el teu dividend:"))
+divisor = int(input("Entra el teu divisor:"))
+Quocient = dividend // divisor
+Residu = dividend % divisor
 print(f"Divisió: {dividend}/{divisor}")
 print(f"Quocient: {Quocient}")
 print(f"Residu: {Residu}")
